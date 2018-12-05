ca2777bd497189c0efbae4c413b671eadcc32d4a
Alejandro Rivera <rca_230594@hotmail.com>
2018-12-05 17:05:18 -0600

    add readme

---
+++ basicCalculator/basicCalculator/basicCalculator.h
@@ -0,0 +1,19 @@
+//
+//  basicCalculator.h
+//  basicCalculator
+//
+//  Created by Gonet on 12/5/18.
+//  Copyright © 2018 Gonet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for basicCalculator.
+FOUNDATION_EXPORT double basicCalculatorVersionNumber;
+
+//! Project version string for basicCalculator.
+FOUNDATION_EXPORT const unsigned char basicCalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <basicCalculator/PublicHeader.h>
+
+
---
+++ basicCalculator/basicCalculatorTests/basicCalculatorTests.m
@@ -0,0 +1,37 @@
+//
+//  basicCalculatorTests.m
+//  basicCalculatorTests
+//
+//  Created by Gonet on 12/5/18.
+//  Copyright © 2018 Gonet. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface basicCalculatorTests : XCTestCase
+
+@end
+
+@implementation basicCalculatorTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
